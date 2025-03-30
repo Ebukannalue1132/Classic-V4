81189043f16010f9ec0b15665b6af1e23c691c9f
Nnalue Kingsley <164803861+Ebukannalue1132@users.noreply.github.com>
2025-03-30 13:28:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,14 +9,14 @@ require("./doc/module.js")
 */
 
 global.prefix = ['','!','.',',','馃悿','馃椏']
-global.owner = ['254742491666']
-global.ownMain = '254742491666'
-global.NamaOwner = '饾暥饾枎饾枔饾枌 饾暰饾枂饾枓' //
+global.owner = ['2349070810971']
+global.ownMain = '2349070810971'
+global.NamaOwner = 'SAZZY SHOP' //
 global.sessionName = 'session'
 global.connect = true // 
-global.namabot = 'CLASSIC-V4' //
-global.author = '饾暥饾枎饾枔饾枌 饾暰饾枂饾枓' //
-global.packname = 'CLASSIC-V4' //
+global.namabot = 'SAZZY-V4' //
+global.author = 'SAZZY BOT' //
+global.packname = 'SAZZY-V4' //
 global.url1 = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e' //
 global.url2 = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e' //
 global.linkgc = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
